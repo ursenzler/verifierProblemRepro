8812d34619ffe411121c3edfda7e3c3c6a371506
Urs Enzler <u.enzler@calitime.ch>
2021-08-03 07:59:50 +0200

    added problem reproduction

---
+++ source/CSharpWorks/VerifierTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using VerifyXunit;
+using Xunit;
+
+namespace CSharpWorks
+{
+    [UsesVerify]
+    public class VerifierTest
+    {
+        [Fact]
+        public async Task Bar()
+        {
+            await Verifier.Verify("works");
+        }
+    }
+}
---
+++ source/VerifierPluginProblemRepro/VerifierTest.fs
@@ -0,0 +1,12 @@
+﻿[<VerifyXunit.UsesVerify>]
+module VerifierPluginProblemRepro.VerifierTest
+
+open VerifyXunit
+open Xunit
+
+let verify (value : 't) =
+    Verifier.Verify<'t>(value).ToTask() |> Async.AwaitTask
+
+[<Fact>]
+let test () =
+    verify "does not work"
